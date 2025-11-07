6105fb709202c93958fbec9580ac3d5a380813c9
Olaf Boekholt <olafboekholt@gmail.com>
2025-11-07 12:14:09 +0000

    benchmark seperation

--- bench/Benchmark.hs
+++ bench/Benchmark.hs
@@ -15,13 +15,30 @@ allOptions :: (Enum a, Bounded a) => [a]
 allOptions = [minBound..maxBound]
 
 main :: IO ()
-main = do
-    let benchmarks = ["pruning"] 
-        defaultConfig = VerifyConfig 20 None False False
+main =
+    completenessBench
+    --heuristicBench
+    
+
+-- Benchmark all variants of a benchmark program with N in [2..10], assuming the files exist in the benchmark dir
+completenessBench :: IO ()
+completenessBench = do
+    let bm = "invalidDivBy"
+        nums = map show [2..10]
+        defaultConfig = VerifyConfig{n=50, ph=None, p=False, se=True}
+
+    progs <- forM nums \file -> (file,) <$> fmap fromRight (parseGCLfile $ "examples/benchmark/" ++ bm ++ file ++ ".gcl")
+
+    defaultMain [bgroup bm $ for progs \(file, p) -> bench file $ nfIO (verifyProgram defaultConfig p)]
+
+-- Benchmark all 6 combinations of (no-)simplify and prune heuristic on multiple benchmarking progams
+heuristicBench :: IO ()
+heuristicBench = do
+    let benchmarks = ["benchmark/invalidBsort"] 
+        defaultConfig = VerifyConfig{n=40, ph=None, p=False, se=False}
         
     progs <- forM benchmarks \file -> (file,) <$> fmap fromRight (parseGCLfile $ "examples/" ++ file ++ ".gcl")
 
-    -- All combinations of (no-)simplify and prune heuristic
     defaultMain $
         for [(p, s) | p <- progs, s <- allOptions] \((file, p), se) -> bgroup (file ++ ", simplify: " ++ show se) $ 
             for allOptions \ph -> bench ("prune heuristic: " ++ show ph) $ 
